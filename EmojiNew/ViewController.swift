d8ec8e9483dd1fc3a04c8ef4776d46939feb1429
QnarikDavtyan <Qnarikdavtian@gmail.com>
2022-05-02 10:25:06 +0400

    Initial Commit

---
+++ EmojiNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiNew
+//
+//  Created by Qnarik Davtyan on 02.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
